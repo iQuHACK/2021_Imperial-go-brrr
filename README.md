ef847d7c40ed1700b6eb4f6c1825e044162f63a1
Xky <Xky@lees-mbp-2.home>
2021-01-31 05:33:00 +0000

    working backend

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
-# Welcome to iQuHACK 2021!
-Check out some info in the [event's repository](https://github.com/iQuHACK/2021) to get started.
+# Quantum Poker
 
-Having a README in your team's repository facilitates judging. A good README contains:
-* a clear title for your project,
-* a short abstract,
-* the motivation/goals for your project,
-* a description of the work you did, and
-* proposals for future work.
+This game is inspired by poker, with a quantum flavour! This game requires 3 players. Each player is assigned a qubit in the quantum circuit, and the objective of the game is to make sure that your qubit is in the $\vert 1 \rangle$ state upon measurement!
 
-You can find a potential README template in [one of last year's projects](https://github.com/iQuHACK/QuhacMan).
+## Card Deck
+In this game, the cards are quantum gates themselves! There are a total of 12 cards and they are:
+* 1 SWAP gate,
+* 1 Toffoli gate,
+* 1 Hadamard gate,
+* 3 Pauli X-gates,
+* 3 CNOT gates,
+* 3 Measure gates.
 
-Feel free to contact the staff with questions over our [event's slack](https://iquhack.slack.com), or via iquhack@mit.edu.
+The cards can be used on the quantum circuit, applying a gate on it with the respective control and target qubits specified by the players.
+  
+## Gameplay
+1. In the beginning, the quantum citcuit is initialised as such:
+![](figures/initial_circuit.png)
+2. Before the round begins, each player is given a Measure gate. Then the rest of the cards are shuffled and split among all players equally. 
+3. The players can only see their own cards. 
+4. At each player's turn, the player must use one card, specifying the card's control and target, when applicable.
+5. The corresponding gate on the card will be applied onto the circuit above.
+6. The circuit is visible to all players at all times.
+7. When all players has taken their turn to put down their cards, the round ends.
+8. All cards can be used at any round, except for the Measure card. The Measure card can only be used after the end of the first round.
+9. The game ends when all players run out of cards, or when a Measure card is put down.
 
-Good luck!
+## How to Win
+
+## Future Work
---
+++ circuit.py
@@ -0,0 +1,108 @@
+#%%
+from importlib import reload
+import gates as gt
+from qiskit import Aer
+from qiskit_ionq_provider import IonQProvider 
+from qiskit import QuantumCircuit
+from qiskit.visualization import plot_histogram
+from qiskit.providers.jobstatus import JobStatus
+import matplotlib.pyplot as plt
+
+#%%
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv('PROJECT_API_KEY')
+#%%
+reload(gt)
+
+class Circuit:
+    def __init__(self, n_qubits=3, API_KEY=API_KEY):
+        self._n_qubits = n_qubits
+        self._gates = []
+        qc = QuantumCircuit(n_qubits, n_qubits)
+        for i in range(n_qubits):
+            qc.h(i)
+        self._circuit = qc
+        self.__API_KEY = API_KEY
+
+    def __repr__(self):
+        return f"{self.get_n_qubits()}-Qubit Circuit, {len(self.get_gates()) + 3} Gates"
+
+    def get_n_qubits(self):
+        return self._n_qubits
+
+    def get_circuit(self):
+        return self._circuit
+
+    def draw(self):
+        return self.get_circuit().draw(output="mpl")
+
+    def get_gates(self):
+        return self._gates
+
+    def apply_gate(self, gate):
+        if isinstance(gate, gt.HadamardGate):
+            self._circuit.h(gate.get_target())
+
+        elif isinstance(gate, gt.XGate):
+            self._circuit.x(gate.get_target())
+
+        elif isinstance(gate, gt.CNOTGate):
+            self._circuit.cx(gate.get_control(), gate.get_target())
+
+        elif isinstance(gate, gt.ToffoliGate):
+            self._circuit.ccx(gate.get_control()[0], gate.get_control()[1], gate.get_target())
+
+        elif isinstance(gate, gt.SWAPGate):
+            self._circuit.swap(gate.get_target()[0], gate.get_target()[1])
+
+        elif isinstance(gate, gt.MeasureGate):
+            self._circuit.measure([i for i in range(self.get_n_qubits())], [i for i in range(self.get_n_qubits())])
+        
+        self._gates.append(gate)
+        return self
+
+    def simulate(self):
+        provider = IonQProvider(token=self.__API_KEY)
+        backend = provider.get_backend("ionq_simulator")
+        job = backend.run(self.get_circuit(), shots=100)
+        return job.result()
+
+    def run(self):
+        provider = IonQProvider(token=self.__API_KEY)
+        backend = provider.get_backend("ionq_qpu")
+        job = backend.run(self.get_circuit(), shots=1)
+        return job.result()
+
+#%%
+# h = gt.HadamardGate(0)
+# x = gt.XGate(1)
+# cnot = gt.CNOTGate(1, 2)
+# tof = gt.ToffoliGate([0, 2], 1)
+# measure = gt.MeasureGate()
+# swap = gt.SWAPGate([1, 2])
+# #%%
+# c = ct.Circuit(API_KEY=API_KEY)
+# fig = c.draw()
+# fig.savefig("figures/initial_circuit.png")
+# #%%
+# c.apply_gate(h)
+# c.apply_gate(cnot)
+# c.apply_gate(x)
+# c.apply_gate(tof)
+# c.apply_gate(swap)
+# c.apply_gate(measure)
+# c.draw()
+# #%%
+# result = c.simulate()
+# plot_histogram(result.get_counts())
+# #%%
+# result = c.run()
+# #%%
+# img = c.draw()
+# type(img)
+# #%%
+
+
+# #%%
